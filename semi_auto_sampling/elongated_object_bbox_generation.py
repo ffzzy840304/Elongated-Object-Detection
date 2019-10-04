4ff2dda7199bdede7890cf5c5a0c34231702cb14
ffzzy840304 <56105355+ffzzy840304@users.noreply.github.com>
2019-10-04 08:59:57 +0800

    Update elongated_object_bbox_generation.py

--- semi_auto_sampling/elongated_object_bbox_generation.py
+++ semi_auto_sampling/elongated_object_bbox_generation.py
@@ -24,7 +24,7 @@ class Point:
     def __repr__(self):
         return "".join(["Point(", str(self.x), ",", str(self.y), ")"])
     
-def Crackwidth(im):
+def Objectwidth(im):
     #im=cv2.imread(imagename,0)
     ret, im = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY)
     element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3,3))
@@ -39,7 +39,7 @@ def Crackwidth(im):
 	    erode = cv2.erode(im,element)
 	    temp  = cv2.dilate(erode, element)
  
-	    #消失的像素是skeleton的一部分
+	    
 	    temp = cv2.subtract(im, temp)
 	    #cv2.imshow('skeleton part %d' %(i,), temp)
 	    skel = cv2.bitwise_or(skel, temp)
@@ -52,9 +52,9 @@ def Crackwidth(im):
     return c-1
 
 #dir_size='G:\\coco\\statistic\\narrow_object_line_fitting\\toothbrush\\bbox_size\\val\\8'
-dir_label='G:\\crack_project\\Tectus\\adaptive_sampling\\labels\\'
-dir_source='G:\\crack_project\\Tectus\\adaptive_sampling\\images\\'
-dir_txt='G:\\crack_project\\Tectus\\adaptive_sampling\\txt_bbox\\'
+dir_label='G:\\elongated_project\\FF\\adaptive_sampling\\labels\\'
+dir_source='G:\\elongated_project\\FF\\adaptive_sampling\\images\\'
+dir_txt='G:\\elongated_project\\FF\\adaptive_sampling\\txt_bbox\\'
 
 imagelist=[]
 sourceimage_list=[]
@@ -73,15 +73,15 @@ for fn in filenames:
         sourceimage_list.append(fullfilename)
 
 
-crack_pixels=[]
-#noncrack_pixels=[]
+object_pixels=[]
+#nonobject_pixels=[]
 for k in range(0,len(imagelist)):
     index=imagelist[k].rfind('\\')
     print imagelist[k]
     img=cv2.imread(dir_label+imagelist[k][index+1:],0)    
     src_img=cv2.imread(dir_source+imagelist[k][index+1:])
-    #set bouding box radius based on calculated crack width
-    width=Crackwidth(img)
+    #set bouding box radius based on calculated object width
+    width=Objectwidth(img)
     r=int(width*1.2)+15
     print r
     rows,cols=img.shape
@@ -89,28 +89,28 @@ for k in range(0,len(imagelist)):
         for j in range (cols):
             p=Point(j,i)
             if(img[i,j]>128):                
-                crack_pixels.append(p) 
+                object_pixels.append(p) 
                 img[i,j]=255
             
-    random.shuffle(crack_pixels)
-    #random.shuffle(noncrack_pixels)    
+    random.shuffle(object_pixels)
+    #random.shuffle(nonobject_pixels)    
 
-    seg_num=round(len(crack_pixels)/width)
-    crack_seg=[]
-    #noncrack_seg=[]
+    seg_num=round(len(object_pixels)/width)
+    object_seg=[]
+    #nonobject_seg=[]
     seg_count=0
-    for i in range(0,len(crack_pixels)):
+    for i in range(0,len(object_pixels)):
         if seg_num==0:
             break;
-        px=crack_pixels[i].x
-        py=crack_pixels[i].y
+        px=object_pixels[i].x
+        py=object_pixels[i].y
         if(px>=r)and(px<=(cols-r))and(py>=r)and(py<=(rows-r)):
-            crack_seg.append(crack_pixels[i])
+            object_seg.append(object_pixels[i])
             seg_count=seg_count+1
             if(seg_count==seg_num):
                 break
            
-    seg_num=round(len(crack_pixels)/2)
+    seg_num=round(len(object_pixels)/2)
     seg_count=0
    
     #do crop now
@@ -120,25 +120,25 @@ for k in range(0,len(imagelist)):
     new_img=src_img.copy()
     seg_count=0
     
-    for pix in crack_seg:
+    for pix in object_seg:
         crop_img=img[pix.y-r:pix.y+r,pix.x-r:pix.x+r]
         crop_srcimg=src_img[pix.y-r:pix.y+r,pix.x-r:pix.x+r]        
         cv2.rectangle(new_img,(pix.x-r,pix.y-r),(pix.x+r,pix.y+r),(0,0,255),1)
         
         fsave=open(txt_savename,'a+')
-        fsave.writelines(str(pix.x-r)+' '+str(pix.y-r)+' '+str(pix.x+r)+' '+str(pix.y+r)+' '+'crack')
+        fsave.writelines(str(pix.x-r)+' '+str(pix.y-r)+' '+str(pix.x+r)+' '+str(pix.y+r)+' '+'baseball_bat')
         fsave.writelines('\n')
         fsave.close()
         
         seg_count=seg_count+1      
 
         
-    #to check whether crack patch cropping correct or not
-    cv2.imwrite('G:\\crack_project\\Tectus\\adaptive_sampling\\source_bb\\'+imagelist[k][index+1:],new_img)
+    #to check whether object patch cropping correct or not
+    cv2.imwrite('G:\\elongated_project\\FF\\adaptive_sampling\\source_bb\\'+imagelist[k][index+1:],new_img)
     
     print 'done' 
-    crack_pixels[:]=[]
-    #noncrack_pixels[:]=[]
+    object_pixels[:]=[]
+    #nonobject_pixels[:]=[]
    
 
 print 'all croping done'
